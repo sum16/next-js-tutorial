132317281dde2afc089bc9fc9ebcc9bd3b37c7fa
yasuyuki sato <godasper@gmail.com>
2021-12-05 02:06:23 +0900

    Linkを使って画面遷移をする

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,15 @@
+import Link from "next/link";
+
 // 必ず default エクスポートする必要がある
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <h1>First Post</h1>;
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
 }
